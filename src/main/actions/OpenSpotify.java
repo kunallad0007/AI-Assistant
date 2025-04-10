0a3fd1c02bae5664faa6d081fc0d5bf63bafe229
kunallad0007 <kunallad0007@gmail.com>
2025-04-11 03:12:40 +0530

    Spotify app was not opening fixed the error on line number 10 and change the msg from opening spotify to Launching Spotify app

--- src/main/actions/OpenSpotify.java
+++ src/main/actions/OpenSpotify.java
@@ -7,8 +7,8 @@ public class OpenSpotify{
     public void execute(){
 
         try{
-            new ProcessBuilder("cmd","/c","open spotify").start();
-            System.out.println("Opening Spotify...");
+            new ProcessBuilder("cmd","/c","start spotify").start();
+            System.out.println("Launching Spotify app...");
         }catch(IOException e){
             System.err.println("Sorry, I didn't understand you !");
         }
